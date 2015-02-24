acc913050d3db8853ff2dae7aad4b48f12386bd0
Nathan Carter <nc8510@mcla.edu>
2015-02-24 15:47:51 -0500

    Added user model and user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
  
-  get 'users/new'
-
 root             'static_pages#home'
 get 'help'    => 'static_pages#help'
 get 'event'   => 'static_pages#event'
+get 'gallery' => 'static_pages#gallery'
+get 'signup'  => 'users#new'
+
+resources :users
 
 end
