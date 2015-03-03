bfe2fc3fcc31f029f9774e71603ca7fe207e6d21
Nathan Carter <nc8510@mcla.edu>
2015-03-03 10:20:01 -0500

    Finished signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    	log_in @user
-    	flash[:success] = "Welcome to the Draft!"
+    	flash[:success] = "Welcome Admin!"
       	redirect_to root_path
     else
       render 'new'
