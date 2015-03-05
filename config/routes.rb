fa5af8d40716d591c2dd7b7209e64e0319c667d9
Nathan Carter <nc8510@mcla.edu>
2015-03-05 10:56:36 -0500

    Finished login and added static edit pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
- 
+
 root             'static_pages#home'
-get 'help'    => 'static_pages#help'
-get 'event'   => 'static_pages#event'
-get 'gallery' => 'static_pages#gallery'
-get 'signup'  => 'users#new'
+get    'help'    => 'static_pages#help'
+get    'event'   => 'static_pages#event'
+get    'gallery' => 'static_pages#gallery'
+get    'edit'    => 'static_pages#edit_events'
+get    'edit_event'    => 'static_pages#edit_event'
+get    'signup'  => 'users#new'
+get    'login'   => 'sessions#new'
+post   'login'   => 'sessions#create'
+delete 'logout'  => 'sessions#destroy'
+
 
 resources :users
 
